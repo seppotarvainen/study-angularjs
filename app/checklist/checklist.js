75a2e8f975bdfa81edf472c0950889c24962b7ac
Seppo Tarvainen <seppo.v.tarvainen@jyu.fi>
2017-12-31 17:31:32 +0200

    Add checklist component

---
+++ app/checklist/checklist.js
@@ -0,0 +1,11 @@
+/**
+ * Created by tarva on 31.12.2017.
+ */
+
+function ChecklistController() {}
+
+angular.module('myApp').component('checklist', {
+    templateUrl: 'checklist/checklist.html',
+    controller: ChecklistController,
+    bindings: {}
+});
